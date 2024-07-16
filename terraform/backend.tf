5dcd9d01abde92e3cbf6a00a41eccf6116412451
Sumit Chavan <sumitchavan378@gmail.com>
2024-07-16 16:18:56 +0530

    Removed files

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-bucket18813093"
-    key            = "terraform.tfstate"
-    region         = "us-east-2"
-    dynamodb_table = "terraform-lock-table"
-    encrypt        = true
-  }
-}
